f626cb8c198a0a30ffdbde634098895155890701
Katharina Hembach <3229704+khembach@users.noreply.github.com>
2018-06-07 15:28:57 +0200

    Fix indentation

--- scripts/add_novel_exon_to_gtf.R
+++ scripts/add_novel_exon_to_gtf.R
@@ -28,16 +28,30 @@ exon_pred <- fread(EXONPRED)
 get_transcripts <- function(pred, exons){
   terminal <- 0
   ## if the lend is NA, this means the exon is terminal
-  if(!is.na(pred[2]) ){
+  if (!is.na(pred[2])){
   ## find all exons that end in lend
-    id1 <- mcols( subsetByOverlaps(exons, GRanges(seqnames = pred[1], ranges = IRanges(as.numeric(pred[2]), as.numeric(pred[2]) ), strand = pred[6]), type = "end", ignore.strand = FALSE) )$transcript_id
-  } else{
+    id1 <- mcols( subsetByOverlaps(exons,
+                                   GRanges(seqnames = pred[1],
+                                           ranges = IRanges(as.numeric(pred[2]),
+                                                            as.numeric(pred[2]) ),
+                                           strand = pred[6]),
+                                   type = "end",
+                                   ignore.strand = FALSE)
+    )$transcript_id
+  } else {
     terminal <- 1
   }
 
   if(!is.na(pred[5])){
     ## find all exons that start in rstart
-    id2 <- mcols( subsetByOverlaps(exons, GRanges(seqnames = pred[1], ranges = IRanges(as.numeric(pred[5]), as.numeric(pred[5])), strand = pred[6]), type = "start", ignore.strand = FALSE) )$transcript_id
+    id2 <- mcols( subsetByOverlaps(exons,
+                                   GRanges(seqnames = pred[1],
+                                           ranges = IRanges(as.numeric(pred[5]),
+                                                            as.numeric(pred[5])),
+                                           strand = pred[6]),
+                                   type = "start",
+                                   ignore.strand = FALSE)
+    )$transcript_id
   } else{
     terminal <- 2
   }
@@ -48,22 +62,40 @@ get_transcripts <- function(pred, exons){
     tr <- intersect(id1, id2)
     if(length(tr) == 0 ) NA
 
-    olap_tr <- mcols( subsetByOverlaps( exons[ mcols(exons)$transcript_id %in% tr ], GRanges(seqnames = pred[1], ranges = IRanges(as.numeric(pred[2]) + 1, as.numeric(pred[5])-1  ), strand = pred[6]), ignore.strand = FALSE) )$ transcript_id
+    olap_tr <- mcols( subsetByOverlaps(exons[ mcols(exons)$transcript_id %in% tr ],
+                                       GRanges(seqnames = pred[1],
+                                               ranges = IRanges(as.numeric(pred[2]) + 1,
+                                                                as.numeric(pred[5])-1  ),
+                                               strand = pred[6]),
+                                       ignore.strand = FALSE)
+    )$transcript_id
     return( tr[! tr %in% olap_tr] ) ## all transcripts that do not have an exon in the region where the new exon will be located
-  } else if( terminal == 1) {  ## 5' terminal exon 
+  } else if( terminal == 1) {  ## 5' terminal exon
   ###### TODO: make sure that we only keep transcripts that start with the overlapping exons!!
 
     tr <- unique(id2)
     if(length(tr) == 0 ) NA
 
-    olap_tr <- mcols( subsetByOverlaps( exons[ mcols(exons)$transcript_id %in% tr ], GRanges(seqnames = pred[1], ranges = IRanges(as.numeric(pred[3]), as.numeric(pred[5])-1 ), strand = pred[6]), ignore.strand = FALSE) )$ transcript_id   
+    olap_tr <- mcols( subsetByOverlaps(exons[ mcols(exons)$transcript_id %in% tr ],
+                                       GRanges(seqnames = pred[1],
+                                               ranges = IRanges(as.numeric(pred[3]),
+                                                                as.numeric(pred[5])-1 ),
+                                               strand = pred[6]),
+                                       ignore.strand = FALSE)
+    )$transcript_id
     return(tr[! tr %in% olap_tr] )
   } else{  ## 3' terminal exon
 
     tr <- unique(id1)
     if(length(tr) == 0 ) NA
     ## remove all transcripts that have an exon in the connected intron or the novel exon
-    olap_tr <- mcols( subsetByOverlaps( exons[ mcols(exons)$transcript_id %in% tr ], GRanges(seqnames = pred[1], ranges = IRanges(as.numeric(pred[2])+1, as.numeric(pred[4]) ), strand = pred[6]), ignore.strand = FALSE) )$ transcript_id   
+    olap_tr <- mcols( subsetByOverlaps(exons[ mcols(exons)$transcript_id %in% tr ],
+                                       GRanges(seqnames = pred[1],
+                                               ranges = IRanges(as.numeric(pred[2])+1,
+                                                                as.numeric(pred[4]) ),
+                                               strand = pred[6]),
+                                       ignore.strand = FALSE)
+    )$ transcript_id
     return(tr[! tr %in% olap_tr] )
   }
 
@@ -80,9 +112,9 @@ tr_ids <- apply(as.data.frame(exon_pred) , 1, function(x) get_transcripts(x, exo
 
 # ### 5' terminal exon: exon_pred[51,]  gets inserted in intron but it is a terminal exon!!!
 # pred <- unlist( exon_pred[51,] )
-# olap_tr <- mcols( 
-#   subsetByOverlaps( exons[ mcols(exons)$transcript_id %in% tr ], GRanges(seqnames = pred[1], ranges = IRanges(as.numeric(pred[3]), as.numeric(pred[5])-1 ), strand = pred[6]), ignore.strand = FALSE) 
-#   )$ transcript_id   
+# olap_tr <- mcols(
+#   subsetByOverlaps( exons[ mcols(exons)$transcript_id %in% tr ], GRanges(seqnames = pred[1], ranges = IRanges(as.numeric(pred[3]), as.numeric(pred[5])-1 ), strand = pred[6]), ignore.strand = FALSE)
+#   )$ transcript_id
 
 ### remove all transcripts with exon starts
 ##### INSERT END
@@ -96,21 +128,32 @@ tr_ids <- apply(as.data.frame(exon_pred) , 1, function(x) get_transcripts(x, exo
 new_transcript <- function(pred, tr_ids, exons){
   type <- ifelse( (pred$end-pred$start+1) <28, "me", "exon")
   new_entries <- GRanges()
-  
+
   tr_copy <- exons[ which( mcols(exons)$transcript_id  %in% tr_ids)]
   # mcols(tr_copy)$"exon_id" <- paste0( mcols(tr_copy)$"exon_id", "_", type, "_", pred$start, ":", pred$end )
-  mcols(tr_copy)$"transcript_id" <- paste0( mcols(tr_copy)$"transcript_id", "_", type, "_", pred$start, ":", pred$end )
-  mcols(tr_copy)$"transcript_name" <- paste0( mcols(tr_copy)$"transcript_name", "_", type, "_", pred$start, ":", pred$end )
+  mcols(tr_copy)$"transcript_id" <- paste0( mcols(tr_copy)$"transcript_id",
+                                           "_", type, "_", pred$start, ":",
+                                           pred$end )
+  mcols(tr_copy)$"transcript_name" <- paste0( mcols(tr_copy)$"transcript_name",
+                                              "_", type, "_", pred$start, ":",
+                                               pred$end )
 
-  copy <- tr_copy[  match(paste0( tr_ids, "_", type, "_", pred$start, ":", pred$end ),  mcols(tr_copy)$transcript_id )]
+  copy <- tr_copy[  match(paste0( tr_ids, "_", type, "_", pred$start, ":",
+                                 pred$end ),
+                          mcols(tr_copy)$transcript_id )]
   ranges(copy) <- IRanges(pred$start, pred$end)  ## replace the ranges
   mcols(copy)[c("exon_number",  "exon_version")] <- NA  ## remove the exon id, number and version
-  mcols(copy)$"exon_id" <- paste0( mcols(copy)$"exon_id", "_", type, "_", pred$start, ":", pred$end )
+  mcols(copy)$"exon_id" <- paste0( mcols(copy)$"exon_id", "_", type, "_",
+                                  pred$start, ":", pred$end )
 
   return(c(tr_copy, copy) )
 }
 
-novel_entries <- lapply(seq_along(tr_ids), function(x) if(length(tr_ids[[x]])>0){ new_transcript(exon_pred[x],tr_ids[[x]], exons = exons) } else NA )
+novel_entries <- lapply(seq_along(tr_ids), function(x)
+                        if(length(tr_ids[[x]])>0){
+                          new_transcript(exon_pred[x],
+                                         tr_ids[[x]], exons = exons)
+                          } else NA )
 
 
 ## remove the novel exons that do not have a transcript
